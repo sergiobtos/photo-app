8b2da05a5d20e78c0c4c69d5eea6b8711cb26483
Sergio Silva <sergiobtos@hotmail.com>
2023-07-08 12:27:57 -0700

    Installing ejs view

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 8080;
 
 app.use(express.static('public'));
-
 app.listen(port, () => {
- console.log('listening on port ' + port)
+    console.log(`Serving photo app on http://localhost:${port}`)
+});
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index');
 });
